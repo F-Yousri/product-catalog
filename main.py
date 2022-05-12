04ef1d9fe7850bc37f098219ddfb93f94c81e722
Fahd Yousri <fahdyousri@gmail.com>
2022-05-12 23:54:24 +0200

    - adds db connection - adds environment variables parsing

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-from fastapi import FastAPI
+from functools import lru_cache
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel, BaseSettings
+from pymongo import MongoClient
 from typing import List
-from pydantic import BaseModel
+
+
+class Settings(BaseSettings):
+    DB_USERNAME: str = ''
+    DB_PASSWORD: str = ''
+    DB_DATABASE: str = ''
+    class Config:
+        env_file = '.env'
+        env_nested_delimiter = "__"
 
 class Product(BaseModel):
+    _id: str
     title: str
     description: str
     imagePath: str
     unitPrice: float
 
+
+settings = Settings()
+
+client = MongoClient('mongodb://%s:%s@mongo' % (settings.DB_USERNAME, settings.DB_PASSWORD))
+db = client[settings.DB_DATABASE]
+
 app = FastAPI()
 
-fake_product_db = [
-    {
-        "title": "macbook pro 16\"",
-        "description": "Cutting edge technology with solid performance.",
-        "imagePath": "somewhere/on/this/server/or/s3",
-        "unitPrice": 3000.5,
-    }
-]
-
-
 @app.get('/', response_model=List[Product])
-async def index():
-    return fake_product_db
+def index():
+    products = []
+    for product in db.products.find():
+        products.append(Product(**product))
+    return products
